ba6a258236805316a22660dbae8397bc1ed41374
Phunnarat <s6203051613191@email.kmutnb.ac.th>
2022-10-26 12:00:50 +0700

    mysql

--- verify.php
+++ verify.php
@@ -5,43 +5,30 @@
         die();
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>verify</title>
-</head>
-<body>
-    
-    
-    
-    <h1> <center> Login NootNoot</center></h1>
-    <hr>
-    <div align="center">
-        <?php
-            $login=$_POST["login"];
-            $psd=$_POST["pwd"];
-            if($login=="admin" && $psd=="ad1234"){
-                echo "ยินดีต้อนรับคุณ ADMIN";
-                $_SESSION['username']='admin';
-                $_SESSION['role']='a';
-                $_SESSION['id']= session_id();
-            }
-            elseif($login=="member" && $psd=="mem1234"){
-                echo "ยินดีต้อนรับคุณ MEMBER";
-                $_SESSION['username']='member';
-                $_SESSION['role']='m';
-                $_SESSION['id']= session_id();
-            }
-            else{
-                echo "ชื่อบัญชีหรื่อรหัสผ่านไม่ถูกต้อง";
-            }
-        ?>
-        <br><a href="index.php">กลับไปหน้าหลัก</a>
-    </div>
-     
 
-</body>
-</html>
+    <?php
+        $login=$_POST["login"];
+        $psd=$_POST["pwd"];
+        if($login=="admin" && $psd=="ad1234"){            
+            $_SESSION['username']='admin';
+            $_SESSION['role']='a';
+            $_SESSION['id']= session_id();
+            header("location:index.php");
+            die();
+        }
+        elseif($login=="member" && $psd=="mem1234"){
+            
+            $_SESSION['username']='member';
+            $_SESSION['role']='m';
+            $_SESSION['id']= session_id();
+            header("location:index.php");
+            die();
+        }
+        else{
+            $_SESSION['error']='error';
+            header("location:login.php");
+            die();
+        }
+    ?>
+
+
